581351d172bc9bd36b293f2364aa46de21829cda
Bystroushaak <bystrousak@kitakitsune.org>
2015-04-28 15:29:43 +0200

    Added .__repr__() to Registrar, added new object DigitalLibrary.

--- src/urn_nbn_api/api_structures/registrar.py
+++ src/urn_nbn_api/api_structures/registrar.py
@@ -15,6 +15,22 @@ class Modes(object):
         self.by_reservation = by_reservation
 
 
+class DigitalLibrary(object):
+    def __init__(self, uid, name, description=None, url=None, created=None):
+        self.url = url
+        self.uid = uid
+        self.name = name
+        self.created = created
+        self.description = description
+
+        # <digitalLibrary id="14">
+        #     <name>Kramerius 3</name>
+        #     <description>předchozí verze knihovny Kramerius</description>
+        #     <url>http://kramerius3.mzk.cz</url>
+        #     <created>2012-04-11T11:15:37.660+02:00</created>
+        # </digitalLibrary>
+
+
 class Registrar(object):
     def __init__(self, code, uid, name=None, description=None, created=None,
                  modified=None, modes=None):
@@ -26,8 +42,10 @@ class Registrar(object):
         self.description = description
 
         self.modes = modes if modes else Modes()
+        self.digital_libraries = []
 
-
+    def __repr__(self):
+        return "%s:%s" % (self.__class__.__name__, self.code)
 
         # <registrar code="boa001" id="8">
         #     <name>MZK</name>
