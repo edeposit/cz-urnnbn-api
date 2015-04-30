6812ac4c6bc90e5a1ec6f95610f53e3e6f794a35
Bystroushaak <bystrousak@kitakitsune.org>
2015-04-30 15:09:13 +0200

    #4: Added tests of Modes class.

---
+++ tests/api_structures/test_modes.py
@@ -0,0 +1,49 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Interpreter version: python 2.7
+#
+# Imports =====================================================================
+import pytest
+
+from urn_nbn_api.api_structures import Modes
+
+
+# Tests =======================================================================
+def test_modes_creation():
+    m = Modes(
+        by_resolver=True,
+        by_registrar=True,
+        by_reservation=True,
+    )
+
+    assert m.by_resolver
+    assert m.by_registrar
+    assert m.by_reservation
+
+
+    m = Modes(
+        by_resolver=False,
+        by_registrar=True,
+        by_reservation=False,
+    )
+
+    assert not m.by_resolver
+    assert m.by_registrar
+    assert not m.by_reservation
+
+
+def test_modes_op_equal():
+    m = Modes(
+        by_resolver=True,
+        by_registrar=True,
+        by_reservation=True,
+    )
+
+    m2 = Modes(
+        by_resolver=False,
+        by_registrar=True,
+        by_reservation=False,
+    )
+
+    assert m != m2
