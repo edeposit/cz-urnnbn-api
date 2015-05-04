7e846293be13bb7736604d54f4294107b1ce2bb7
Bystroushaak <bystrousak@kitakitsune.org>
2015-05-04 15:33:48 +0200

    #4: Added tests of test_both_set_and_different().

--- tests/api_structures/test_tools.py
+++ tests/api_structures/test_tools.py
@@ -24,3 +24,12 @@ def test_to_list():
 
     assert tools.to_list([1]) == [1]
     assert tools.to_list((1,)) == (1,)
+
+
+def test_both_set_and_different():
+    assert not tools.both_set_and_different(None, 1)
+    assert not tools.both_set_and_different(1, None)
+    assert not tools.both_set_and_different(None, None)
+
+    assert not tools.both_set_and_different(1, 1)
+    assert tools.both_set_and_different(1, 2)
