648245e5caf4b9d38bd3a13b9a85a1bd6873c587
Bystroushaak <bystrousak@kitakitsune.org>
2015-05-14 16:52:08 +0200

    #10: Fixed imports after module renaming.

--- src/urn_nbn_api/__init__.py
+++ src/urn_nbn_api/__init__.py
@@ -4,5 +4,5 @@
 # Interpreter version: python 2.7
 #
 # Imports =====================================================================
-from xml_composer import compose_mono_xml
-from xml_composer import compose_mono_volume_xml
+from xml_convertor import compose_mono_xml
+from xml_convertor import compose_mono_volume_xml
