3d674791316b9e90753cad9d3896afd4b4ee5e1e
Bystroushaak <bystrousak@kitakitsune.org>
2015-05-15 17:57:12 +0200

    #10: Added MultiMonoComposer, some functions converted to static methods.

--- src/urn_nbn_api/xml_composer.py
+++ src/urn_nbn_api/xml_composer.py
@@ -13,36 +13,11 @@ See:
 """
 # Imports =====================================================================
 import xmltodict
-import dhtmlparser
 
 from odictliteral import odict
 
 
 # Functions & classes =========================================================
-def _create_path(root, dict_type, path):
-    """
-    Create nested dicts in `root` using `dict_type` as constructor. Keys from
-    `path` are used to construct the keys for nested dicts.
-
-    Args:
-        root (dict instance): Root dictionary, where the nested dicts will be
-             created.
-        dict_type (dict-like class): Class which will be used to construct
-                  dicts - ``dict_type()`.
-        path (list/tuple of strings): List of keys for nested ``dict_type``.
-
-    Returns:
-        dict: Return last nested dict (dict at last element at `path`).
-    """
-    for sub_path in path:
-        if not isinstance(root.get(sub_path, None), dict):
-            root[sub_path] = dict_type()
-
-        root = root[sub_path]
-
-    return root
-
-
 class MonographComposer(object):
     def __init__(self, **kwargs):
         self.title = None
@@ -58,13 +33,42 @@ class MonographComposer(object):
         self.year = None
         self.format = None
 
+        self._kwargs_to_attributes(kwargs)
+
+    def _kwargs_to_attributes(self, kwargs):
         for key, val in kwargs.iteritems():
             if key in self.__dict__:
                 self.__dict__[key] = val
             else:
                 raise ValueError("Can't set %s parameter!" % key)
 
-    def _assign_pattern(self, where, key, what):
+    @staticmethod
+    def _create_path(root, dict_type, path):
+        """
+        Create nested dicts in `root` using `dict_type` as constructor. Strings
+        in `path` are used to construct the keys for nested dicts.
+
+        Args:
+            root (dict instance): Root dictionary, where the nested dicts will
+                 be created.
+            dict_type (dict-like class): Class which will be used to construct
+                      dicts - ``dict_type()`.
+            path (list/tuple of strings): List of keys for nested
+                 ``dict_type``.
+
+        Returns:
+            dict: Return last nested dict (dict at last element at `path`).
+        """
+        for sub_path in path:
+            if not isinstance(root.get(sub_path, None), dict):
+                root[sub_path] = dict_type()
+
+            root = root[sub_path]
+
+        return root
+
+    @staticmethod
+    def _assign_pattern(where, key, what):
         if what:
             where[key] = what
 
@@ -88,7 +92,7 @@ class MonographComposer(object):
         mono_root = root["r:import"]["r:monograph"]
 
         # subtitle
-        self._assign_pattern(
+        MonographComposer._assign_pattern(
             mono_root["r:titleInfo"],
             "r:subTitle",
             self.subtitle
@@ -100,7 +104,11 @@ class MonographComposer(object):
         self._add_identifier_to_mono(mono_root, "other_id", out="otherId")
 
         # add form of the book
-        self._assign_pattern(mono_root, "r:documentType", self.document_type)
+        MonographComposer._assign_pattern(
+            mono_root,
+            "r:documentType",
+            self.document_type
+        )
 
         mono_root["r:digitalBorn"] = self.digital_born
 
@@ -113,14 +121,18 @@ class MonographComposer(object):
         if any([self.place, self.publisher, self.year]):
             publ = odict()
 
-            self._assign_pattern(publ, "r:publisher", self.publisher)
-            self._assign_pattern(publ, "r:place", self.place)
-            self._assign_pattern(publ, "r:year", self.year)
+            MonographComposer._assign_pattern(
+                publ,
+                "r:publisher",
+                self.publisher
+            )
+            MonographComposer._assign_pattern(publ, "r:place", self.place)
+            MonographComposer._assign_pattern(publ, "r:year", self.year)
 
             mono_root["r:publication"] = publ
 
         if self.format:
-            format_dict = _create_path(
+            format_dict = MonographComposer._create_path(
                 root,
                 odict,
                 [
@@ -149,3 +161,42 @@ class MonographComposer(object):
 
     def __str__(self):
         return self.to_xml()
+
+
+class MultiMonoComposer(MonographComposer):
+    def __init__(self, **kwargs):
+        self.volume_title = None
+
+        super(MultiMonoComposer, self).__init__()
+        self._kwargs_to_attributes(kwargs)
+
+    @staticmethod
+    def _swap_keys(ordered_dict, old_key, new_key):
+        return odict(
+            (new_key, val) if key == old_key else (key, val)
+            for key, val in ordered_dict.iteritems()
+        )
+
+    def to_xml_dict(self):
+        root = super(MultiMonoComposer, self).to_xml_dict()
+
+        # same as mono, except that <r:monograph> is now <r:monographVolume>
+        root["r:import"] = MultiMonoComposer._swap_keys(
+            root["r:import"],
+            "r:monograph",
+            "r:monographVolume"
+        )
+
+        # same as mono, except that <r:title> is now <r:monographTitle>
+        mono_volume = root["r:import"]["r:monographVolume"]
+        mono_volume["r:titleInfo"] = MultiMonoComposer._swap_keys(
+            mono_volume["r:titleInfo"],
+            "r:title",
+            "r:monographTitle"
+        )
+
+        title_info = root["r:import"]["r:monographVolume"]["r:titleInfo"]
+        if self.volume_title:
+            title_info["r:volumeTitle"] = self.volume_title
+
+        return root
