b7898732c8f44dfdaff3a079f103667ad3a0d915
Bystroushaak <bystrousak@kitakitsune.org>
2015-05-15 18:29:01 +0200

    #10: Added imports for XML composers.

--- src/urn_nbn_api/__init__.py
+++ src/urn_nbn_api/__init__.py
@@ -4,5 +4,8 @@
 # Interpreter version: python 2.7
 #
 # Imports =====================================================================
-from xml_convertor import compose_mono_xml
-from xml_convertor import compose_mono_volume_xml
+from xml_composer import MonographComposer
+from xml_composer import MultiMonoComposer
+
+from xml_convertor import convert_mono_xml
+from xml_convertor import convert_mono_volume_xml
