569db0de5326721b71fbf6b4b61ada4efeddb740
Bystroushaak <bystrousak@kitakitsune.org>
2015-05-18 17:49:12 +0200

    #4: Removed unused parts of test_tools.py.

--- tests/api_structures/test_tools.py
+++ tests/api_structures/test_tools.py
@@ -9,15 +9,6 @@ import pytest
 from urn_nbn_api.api_structures import tools
 
 
-# Fixtures ====================================================================
-# @pytest.fixture
-# def fixture():
-#     pass
-
-# with pytest.raises(Exception):
-#     raise Exception()
-
-
 # Tests =======================================================================
 def test_to_list():
     assert tools.to_list(1) == [1]
