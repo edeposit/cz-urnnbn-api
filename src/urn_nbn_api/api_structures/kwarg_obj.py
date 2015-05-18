f98465c6404cefa60c59f3c794df3a28785e5b05
Bystroushaak <bystrousak@kitakitsune.org>
2015-05-18 17:57:28 +0200

    #5: Added docstring for KwargObj.

--- src/urn_nbn_api/api_structures/kwarg_obj.py
+++ src/urn_nbn_api/api_structures/kwarg_obj.py
@@ -8,6 +8,40 @@
 
 # Functions & classes =========================================================
 class KwargObj(object):
+    """
+    This class defines method to map kwargs to attributes, so you can just
+    call :meth:`_kwargs_to_attributes` in your ``__init__``.
+
+    For example::
+
+        class Xex(object):
+            def __init__(self, something, **kwargs):
+                self.something = something
+                self.something_else = None
+                self.something_different = None
+
+                self._kwargs_to_attributes(kwargs)
+
+    This will allow to pass parameters which sets also ``something_else`` and
+    ``something_different``.
+
+    There is also modified :meth:`__setattr__`` method, which disables to set
+    new attributes. This may be good idea for data containers and typos.
+
+    Modified :meth:`__setattr__` functionality can be triggered by setting the
+    ``_all_set`` attribute::
+
+        class Xex(object):
+            def __init__(self):
+                self.something = None
+                self.something_else = None
+                self.something_different = None
+
+                self._all_set = True
+
+    It will be now impossible to create new attribute, which may be good for
+    handling typos. You can still redefine already defined attributes!
+    """
     def __setattr__(self, name, value):
         """
         Disable setting values which are not defined in ``.__init__()``.
