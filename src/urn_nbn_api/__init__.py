e4b0d30942c7841415b77f579e786e46d531994d
Bystroushaak <bystrousak@kitakitsune.org>
2015-05-22 11:46:57 +0200

    Added import of all structures to package level.

--- src/urn_nbn_api/__init__.py
+++ src/urn_nbn_api/__init__.py
@@ -4,8 +4,11 @@
 # Interpreter version: python 2.7
 #
 # Imports =====================================================================
+from api_structures import *
+
 from xml_composer import MonographComposer
 from xml_composer import MultiMonoComposer
 
 from xml_convertor import convert_mono_xml
 from xml_convertor import convert_mono_volume_xml
+
