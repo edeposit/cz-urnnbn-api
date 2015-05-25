7e474b5017f69026f279bb9326d1f4fb7df67262
Bystroushaak <bystrousak@kitakitsune.org>
2015-05-25 18:45:23 +0200

    #15, #9: Added integration testing of some of the API calls. More will come.

--- tests/integration/test_api.py
+++ tests/integration/test_api.py
@@ -5,21 +5,54 @@
 #
 # Imports =====================================================================
 import pytest
+import faker  # fake-factory module
+
+from urn_nbn_api import api
+from urn_nbn_api import settings
+from urn_nbn_api import MonographComposer
 
 
 # Variables ===================================================================
-
-
-
 # Fixtures ====================================================================
-# @pytest.fixture
-# def fixture():
-#     pass
-
-# with pytest.raises(Exception):
-#     raise Exception()
+@pytest.fixture
+def reg_code():
+    return "edep"
 
 
 # Tests =======================================================================
-def test_():
-    pass
+def test_is_valid_reg_code(reg_code):
+    assert api.is_valid_reg_code(reg_code)
+
+
+def test_iter_registrars(reg_code):
+    registrars = list(api.iter_registrars())
+
+    assert registrars
+
+    assert any(reg for reg in registrars if reg.code == reg_code)
+
+
+def test_get_registrar_info(reg_code):
+    reg_info = api.get_registrar_info(reg_code)
+
+    assert reg_info
+    assert reg_info.code == reg_code
+    assert reg_info.name == "Edeposit"
+    assert not reg_info.description
+    assert reg_info.modes == api.Modes(True, True, True)
+
+
+def test_register_document_obj():
+    fake = faker.Factory.create('cz_CZ')
+
+    urn_nbn = api.register_document_obj(
+        MonographComposer(
+            title=fake.sentence(nb_words=3),
+            author=fake.name(),
+            format="pdf"
+        )
+    )
+
+    assert str(urn_nbn)
+
+    assert api.get_gigital_instances(urn_nbn) == []
